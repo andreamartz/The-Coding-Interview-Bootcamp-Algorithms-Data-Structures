71f858ec0d4fe1e5088fb36d9c35b95823508916
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-27 16:20:41 -0500

    Add three working (and one non-working) solutions to reversestring problem

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -2,10 +2,50 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// SOLUTION #1 - probably the interviewer won't let you use .reverse(), though
+// function reverse(str) {
+//   return arr = str.split('').reverse().join('');
+// }
+
+
+// SOLUTION #2
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+
+// SOLUTION #3 -- doesn't work!! because strings are immutable
+// function reverse(str) {
+//   let left = 0;
+//   let right = str.length - 1;
+
+//   while (left < right) {
+//     let temp = str[left];
+//     str[left] = str[right];
+//     str[right] = temp;
+//     console.log(str);
+//     left++;
+//     right--;
+//   }
+//   return str;
+// }
+
+
+// SOLUTION #4 - uses .reduce()!!!
+function reverse(str) {
+  const arr = str.split('');
+  return arr.reduce(((reversed, char) => char + reversed), '');
+}
 
 module.exports = reverse;
