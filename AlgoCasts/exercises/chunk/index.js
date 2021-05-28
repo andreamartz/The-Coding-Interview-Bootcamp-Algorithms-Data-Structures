5af3bbf6f81e9ea0a5649fb6a3f4265f961f1832
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-28 11:52:11 -0500

    Add chunk solution

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -8,6 +8,68 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Edge cases:
+// [] --> []
+
+
+// SOLUTION #1
+// function chunk(array, size) {
+//   // create an array `chunks` to contain the result to return
+//   const chunked = [];
+  
+//   // iterate through the array and place subarrays
+//   for (let el of array) {
+//     const last = chunked[chunked.length - 1];
+//     // if the last element in the array is full or doesn't exist
+//     if (!last || last.length === size ) {
+//       // push onto chunks a new subarray containing the current element from array
+//       chunked.push([el]);
+//     } else {
+//       last.push(el);
+//     }
+//   }
+//   return chunked;
+// }
+
+
+// SOLUTION #2 -- using splice
+// function chunk(array, size) {
+//   // create an array `chunks` to contain the result to return
+//   const chunks = [];
+  
+//   while (array.length > 0) {
+//     let subArray = array.splice(0, size);
+//     chunks.push(subArray);
+//   }
+//   return chunks;
+// }
+
+
+// SOLUTION #3 -- using slice
+// function chunk(array, size) {
+//   const chunked = [];
+//   let idx = 0;
+
+//   while (idx < array.length) {
+//     chunked.push(array.slice(idx, idx + size));
+//     idx += size;
+//   }
+//   return chunked
+// }
+
+// SOLUION #4 (recursive, using splice)
+function chunk(array, size) {
+  // create an array `chunks` to contain the result to return
+  const chunked = [];
+  
+  function chunkHelper() {
+    if (array.length === 0) return chunked;
+
+    chunked.push(array.splice(0, size));
+    return chunkHelper();
+  }
+  return chunkHelper();
+}
+
 
 module.exports = chunk;
