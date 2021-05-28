b4e54de6472a71430d4db07758dea2d6a4e4c3bf
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-28 10:19:07 -0500

    Add maxchar solution

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,36 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Questions to ask:
+// - Are there any characters that should be ignored/no counted (e.g., punctuation, whitespace, etc.)?
+// - Do capital letters and lowercase letters get counted the same or separately?
+// - What should be returned if there is a tie?
+// - What should be returned if ALL of the characters are unique?
+
+// Edge cases:
+// maxChar("        ") === " ";   ??????
+
+// Strategy:
+// - split the string into an array
+// - iterate over the string to create an object containing character counts
+// - keep track of the character with the nax count (and what the value of that count is) and compare each updated count to it
+// - after every character has been counted, return the character with the max count
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let mostUsedChar;
+  
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+    if (charMap[char] > max) {
+      max = charMap[char];
+      mostUsedChar = char;
+    }
+  }
+  return mostUsedChar;
+}
+
+console.log(maxChar("apple 1231111"));
 
 module.exports = maxChar;
