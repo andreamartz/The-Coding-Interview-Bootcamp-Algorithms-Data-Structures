3f1e5b096db67d20f014446bf3d4d1d10c421974
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-28 15:46:13 -0500

    Add two solutions to capitalize

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -7,6 +7,52 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Questions:
+// - how should I handle capital letters that appear in the middle of the word? Should I ignore those or lowercase them?
+// - how should I handle non-string inputs?
+// - how should I 
+
+// Edge cases:
+// - capitalize('1 (one) short phrase') --> what to return???
+// - capitalize(`mcdonald's`);
+
+// Strategy:
+// - split the string on spaces
+// - Iterate through the resulting array and capitalize each word
+// - rejoin the string
+// - return the capitalized string
+
+
+// SOLUTION #1
+// function capitalize(str) {
+//   let capsArr = [];
+//   for (word of str.split(' ')) {
+//     capsArr.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return capsArr.join(' ');
+// }
+
+
+// Strategy:
+// - iterate through the string
+   // - if a space precedes any character, capitalize that character
+
+// Weakness with solution #2 is that it's a bit clumsy how we have to handle capitalizing the first character in the string
+
+// SOLUTION #2
+function capitalize(str) {
+  let capitalized = str[0].toUpperCase();
+  
+  for (let i = 1; i < str.length; i++) {
+    // if character is preceded by a space, capitalize it
+    if (str[i - 1] === ' ') {
+      capitalized += str[i].toUpperCase();
+    } else {
+      capitalized += str[i];
+    }
+  }
+  // return the capitalized string
+  return capitalized;
+}
 
 module.exports = capitalize;
