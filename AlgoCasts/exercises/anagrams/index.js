96fb11534f4bea582f49ff1215607770378a7476
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-28 14:57:03 -0500

    Add two solutions to anagrams challenge

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,72 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// Questions:
+// - are digits considered characters to consider in determination of anagram status?
+// - what to return if both strings are empty (e.g., "")?
+
+// Strategy: 
+// - make all letters lowercase
+// - remove all non-alphanumeric characters
+// - for each string, create an object containing character counts
+// - compare the two objects;
+   // - if the contents are the same, then the two strings are anagrams of each other
+   // - if not, they are NOT anagrams
+
+// Edge cases:
+// - anagrams('hello', 'hellos') --> don't just iterate over stringA's charMap and compare to stringB's charMap bc stringA might be a substring of stringB and that would yield an incorrect result THE FIX: do a check to compare string lengths early on; if different lengths, return false
+
+
+// SOLUTION #1 - LESS efficient bc we are using .sort(), which is O(n log(n)) at best.
+function anagrams(stringA, stringB) {
+  // compare the two cleaned and sorted strings; if not equal, return false
+  if (clean(stringA) !== clean(stringB)) return false;
+
+  return true;
+}
+// helper function
+function clean(str) {
+  // remove all non-alphanumeric characters and make everything lowercase
+  const regex = /[\W_]/g;
+  str = str.replace(regex, "").toLowerCase().split('').sort().join('');
+
+  return str;
+}
+
+// SOLUTION #2 - MORE efficient
+// function anagrams(stringA, stringB) {
+//   [stringA, charMapA] = makeCharMap(stringA);
+//   [stringB, charMapB] = makeCharMap(stringB);
+
+//   // if lengths of the adjusted strings are different, return false
+//   if (stringA.length !== stringB.length) return false;
+
+//   for (key in charMapA) {
+//     console.log(charMapA, charMapB);
+//     if (charMapA[key] !== charMapB[key]) return false;
+//   }
+
+//   return true;
+// }
+
+// // helper function
+// function makeCharMap(str) {
+//   // remove all non-alphanumeric characters and make everything lowercase
+//   const regex = /[\W_]/g;
+//   str = str.replace(regex, "").toLowerCase();
+
+//   // - create an object containing character counts on the adjusted string
+//   const charMap = {};
+//   for (char of str) {
+//     charMap[char] = (charMap[char] += 1) || 1;
+//   }
+
+//   return [str, charMap];
+// }
+
+console.log(anagrams('rail safety', 'fairy tales'));  // True
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales'));  // True
+console.log(anagrams('Hi there', 'Bye there'));  // False
 
 module.exports = anagrams;
