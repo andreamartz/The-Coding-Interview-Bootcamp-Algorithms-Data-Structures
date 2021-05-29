9747d78484066df5a81ca6a2de337c89c56c9945
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-28 20:51:18 -0500

    Add three solutions for pyramid challenge

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+// SOLUTION #1 (nested loops)
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (midpoint - row <= col && midpoint + row >= col) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// SOLUTION #2
+// function pyramid(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(' '.repeat(n - i) + '#'.repeat(2 * i - 1) + ' '.repeat(n - i));
+//   }
+// }
+
+
+// SOLUTION #3 (uses recursion)
+function pyramid(n, idx = 1) {
+  // BASE CASE:
+  if (idx === n + 1) return;
+
+  // RECURSIVE CASE:
+  console.log(' '.repeat(n - idx) + '#'.repeat(2 * idx - 1) + ' '.repeat(n - idx));
+  pyramid(n, idx + 1);
+}
+
 
 module.exports = pyramid;
